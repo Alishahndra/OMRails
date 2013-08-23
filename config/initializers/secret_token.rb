5ff30ea7444cf8704daefc05fa69f84bb81b136c
Alexandra McDougall <alexandramcdougall@gmail.com>
2013-08-22 22:08:36 -0400

    Removing DS Store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OMRails::Application.config.secret_token = '3642d6159e5f73315a8b9152f0dd0884f614d672e8370dde75439a2638c27ed5349165b91012f81684317b806edfc3d3fe09fddbd0a2031e6d456d13ba339e41'
